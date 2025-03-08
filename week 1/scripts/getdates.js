defc067dd35f6698b28e7f59775b632d70f9622f
precious-israel <carisabiodun@gmail.com>
2025-03-08 22:46:36 +0100

    my first commit

---
+++ week 1/scripts/getdates.js
@@ -0,0 +1,3 @@
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+document.getElementById("getdates.js").textContent = "Last Modiified Date " + document.lastModified;
